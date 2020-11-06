fdf9e76abbfaae3ab488dea0644b3bb8629d4451
M-DeJean <dejeantech@yahoo.com>
2020-11-06 15:18:20 -0800

    initializing repo

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Root from './root/Root'
+
+ReactDOM.render(<Root />, document.getElementById('root'))
---
+++ src/root/Root.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Root() {
+  return <div>
+    <h1>Petful</h1>
+  </div>
+}
+
+export default Root
