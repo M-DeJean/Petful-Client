a8f3b23d1bbe715e4b21333c12f21a23090f420d
M-DeJean <dejeantech@yahoo.com>
2020-11-06 15:37:22 -0800

    setting up client environment: config and root

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Root from './root/Root'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<Root />, document.getElementById('root'))
+ReactDOM.render(
+    <BrowserRouter>
+        <Root />
+    </BrowserRouter>
+    , document.getElementById('root'))
