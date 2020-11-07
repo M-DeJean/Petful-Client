42c8c5fc2d7dc883b1c1f339503be930a85775b8
M-DeJean <dejeantech@yahoo.com>
2020-11-06 16:02:24 -0800

    landing page component and route displaying text

--- src/root/Root.js
+++ src/root/Root.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import LandingPage from '../LandingPage/LandingPage'
+import AdoptionPage from '../AdoptionPage/AdoptionPage.js'
 
-function Root() {
-  return <div>
-    <h1>Petful</h1>
-  </div>
+export default class Root extends Component {
+    render() {
+      return (
+        <div className='Root'>
+          <h1>Petful</h1>
+
+          <Switch>
+            <Route
+              exact
+              path={'/'}
+              component={LandingPage}
+            />
+            <Route
+              exact
+              path={'/adoption'}
+              component={AdoptionPage}
+            />
+          </Switch>
+        </div>
+      )
+    }
 }
-
-export default Root
