90bfcecad06ad209a245dfaaddd2e5a7b73ed730
M-DeJean <dejeantech@yahoo.com>
2020-11-06 22:10:24 -0800

    working on pet routes

--- src/AdoptionPage/AdoptionPage.js
+++ src/AdoptionPage/AdoptionPage.js
@@ -1,23 +1,59 @@
 import React, { Component } from 'react'
+
 import ApiService from './../Services/ApiService'
 export default class AdoptionPage extends Component {
 
     state = {
         cats: null,
         dogs: null,
-        people: null
+        people: null,
+        loading: true
     }
 
     componentDidMount() {
-
+        ApiService.getPets()
+            .then(pets => {
+                this.setState({
+                    cats: pets.cats,
+                    dogs: pets.dogs
+                })
+            })
+        ApiService.getPeople()
+            .then(people => {
+                this.setState({ people })
+                this.setState({ loading: false })
+            })
     }
 
+
     render() {
+        const { cats, dogs, people, loading } = this.state
 
         return (
-
-
-            <h1>Adopt a Pet!</h1>
+            <>
+                { loading ? <p>...LOADING PETS...</p> :
+                    <section className='AdoptionPage'>
+                        <h1>Adopt a Pet!</h1>
+                        <div className='cat'>
+                            <h2>Cat</h2>
+                            <img src={cats.imageURL}></img>
+                            <p>Name: {cats.name}</p>
+                            <button>ADOPT</button>
+                        </div>
+                        <div className='dog'>
+                            <h2>Dog</h2>
+                            <img src={dogs.imageURL}></img>
+                            <p>Name: {dogs.name}</p>
+                            <button>ADOPT</button>
+                        </div>
+                        <div className='People'>
+                            <h2>Queue</h2>
+                            {people.map(person =>
+                            <p key={person}>{person}</p>
+                            )}
+                        </div>
+                    </section>}
+            </>
         )
     }
 }
--- src/Services/ApiService.js
+++ src/Services/ApiService.js
@@ -8,5 +8,27 @@ const Apiservice = {
                     ? res.json().then(e => Promise.reject(e))
                     : res.json()
             )
+    },
+    getPeople() {
+        return fetch(`${config.API_ENDPOINT}/people`)
+            .then(res =>
+                (!res.ok)
+                    ? res.json().then(e => Promise.reject(e))
+                    : res.json())
+    },
+    adoptPet(type) {
+        return fetch(`${config.API_ENDPOINT}/pets`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                type: type
+            })
+        })
+        .then(res => res)
     }
+
 }
+
+export default Apiservice
