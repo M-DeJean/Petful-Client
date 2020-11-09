3e822c3eff9551fd6344c8c65309b956f477b6ef
M-DeJean <dejeantech@yahoo.com>
2020-11-08 23:57:58 -0800

    config.js changes to link heroku server

---
+++ build/precache-manifest.19e56f11e0c0596a5b8ad72e79bc4adf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b2f8a3425d603146e9c0a3ff881006",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52d5aa3a687a9c9f99f0",
+    "url": "/static/css/main.381dc735.chunk.css"
+  },
+  {
+    "revision": "c7471a9f14e287bb4051",
+    "url": "/static/js/2.b827be79.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.b827be79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52d5aa3a687a9c9f99f0",
+    "url": "/static/js/main.3bc19b4f.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  }
+]);
