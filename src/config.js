7b6f05e668704fe6fc92fc466f4caa4472ea35bc
M-DeJean <dejeantech@yahoo.com>
2020-12-18 14:27:20 -0800

    changed config api endpoint to heroku for deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api'
-    // API_ENDPOINT: 'https://evening-meadow-30359.herokuapp.com/api'
+    // API_ENDPOINT: 'http://localhost:8000/api'
+    API_ENDPOINT: 'https://evening-meadow-30359.herokuapp.com/api'
 }
